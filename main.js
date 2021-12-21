017e6b4b4838ac899932865949b4ef2dd4b5edb3
xirax <a.gacekx@gmial.com>
2021-12-21 16:56:57 +0100

    saper_END

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const boardGenerator = {
     BOMBS: 0,
     REMAINING: 0,
     ACTIVE: true,
+    ALL_FIELDS: 0, 
 
     BOARD: [],
 
@@ -23,13 +24,14 @@ const boardGenerator = {
 
         this.ACTIVE = false;
 
+        document.getElementById('emoji-icon').src = './imgs/emoji/dead.png';
+        document.getElementById('end-menu').style.display = "flex";
+
         for(let i=0; i<this.HEIGHT; i++){
 
             for(let j=0; j<this.WIDTH; j++){
 
                 if(this.BOARD[i][j] == 10){
-
-                    console.log('FOUND BOMB');
 
                     let ID = 'C_' + i + '_' + j;
 
@@ -59,24 +61,32 @@ const boardGenerator = {
                     document.getElementById(ID).style.backgroundColor = 'red';
     
                     timer.stop();
-                    this.revealBombs();
+                    this.revealBombs(); 
                 }
                 else{
+
+                    document.getElementById('emoji-icon').src = './imgs/emoji/uffP.png';
+
+                    setTimeout(() => { 
+                        if(this.ACTIVE) document.getElementById('emoji-icon').src = './imgs/emoji/happy.png'; 
+                    }, 900);
     
                     if(val == 0){
                         this.setValueAt(hPos, wPos, 'X');
                         this.revealAllAround(hPos, wPos);
             
-                        val = 'X'
+                        val = 'X';
                     } 
             
                     let cls = 'style' + val;
             
                     document.getElementById(ID).innerHTML = val;
                     document.getElementById(ID).classList.add(cls);
-                    document.getElementById(ID).style.backgroundColor = '#e6e6e6';
+                    document.getElementById(ID).style.backgroundColor = '#e6e6e6';  
                 }
             }
+
+            this.checkReveals();
         }
     },
 
@@ -101,6 +111,7 @@ const boardGenerator = {
                 let ID = "C_" + i + "_" + j;
                 let cell = document.createElement('td');
                     cell.id = ID;
+                    cell.classList.add('cell');
 
                     cell.addEventListener('mousedown', (ev) => { 
 
@@ -148,6 +159,30 @@ const boardGenerator = {
     
                 this.remainingBombs(-1);
             }
+        }
+    },
+
+    checkReveals: function(){
+
+        let ctn = 0;
+
+        let fields = document.getElementsByClassName('cell');
+
+        for(let i=0; i<fields.length; i++){
+            if(fields[i].innerHTML != '' && fields[i].innerHTML != '?' && fields[i].innerHTML != '<img class="icon" src="./imgs/flag.png">') ctn++;
+        }
+
+        console.log(ctn);
+
+        if(ctn == (this.ALL_FIELDS - this.BOMBS)){
+
+            timer.stop();
+           // this.revealBombs();
+
+            this.ACTIVE = false;
+            document.getElementById('emoji-icon').src = './imgs/emoji/win.png';
+            document.getElementById('end-menu').style.display = "flex";
+            scores.setRecord(timer.timeSpan(), this.WIDTH, this.HEIGHT, this.BOMBS);
         }
     },
 
@@ -245,16 +280,93 @@ const timer = {
 
     stop: function(){
         clearInterval(this.timer);
+
+        this.END_TIME = Date.now();
+    },
+
+    timeSpan: function(){
+        return this.END_TIME - this.START_TIME;
     }
 }
 
 
-window.addEventListener('DOMContentLoaded', () => {
+const scores = {
+
+    getAll: function(){ 
+        return document.cookie;
+    },
 
 
+    getSortedByTime: function(){
 
-   // timer.start();
-})
+        let cookies = document.cookie;
+
+        if(cookies != '') cookies = cookies.split(';');
+        else cookies = [];
+
+        let players = [];
+        
+        for(let i=0; i<cookies.length; i++){
+
+            let player = cookies[i].split('=')[1].split('_');
+
+            
+            let obj = {
+
+                nick: player[0],
+                time: player[1],
+                mode: player[2]
+            }
+
+            players.push(obj);
+        }  
+
+        players.sort((a, b) => { return a.time - b.time; })
+
+        return players;
+    },
+
+    setRecord: function(time, W, H, B){
+
+        let sorted = this.getSortedByTime();
+
+        let ID = 'PLAYER' + sorted.length;
+
+        let add = false;
+        let moreThan10 = false;
+
+        if(sorted.length >= 10){
+
+            moreThan10 = true;
+
+            for(let i=0; i<sorted.length; i++){
+
+                if(time < sorted[i].time){
+                    add = true;
+                    break;
+                }
+            }
+        }
+        else add = true;
+
+        if(add){
+
+            let nick = prompt("Dostałeś się do tabeli rekordów! Podaj nick");
+
+            let data = nick + '_' + time + '_' + W + 'x' + H + '(' + B + ')';
+
+            let expDate = new Date();
+
+            expDate.setTime(expDate.getTime() + (4*24*60*60*1000));
+
+            let expStr = "expires="+ expDate.toUTCString();
+
+            let coockieStr = ID + "=" + data + ";" + expStr;
+
+            document.cookie = coockieStr;
+        }
+    }
+}
 
 
 function startGame(){
@@ -267,6 +379,7 @@ function startGame(){
     boardGenerator.HEIGHT = H == '' ? 10 : H;
     boardGenerator.BOMBS = B == '' ? 10 : B;
     boardGenerator.REMAINING = boardGenerator.BOMBS;
+    boardGenerator.ALL_FIELDS = W * H;
 
     boardGenerator.generateHTML();
     boardGenerator.generateBombs();
@@ -280,6 +393,75 @@ function startGame(){
     document.getElementById('board').style.display = 'block';
     document.getElementById('TOP').style.display = 'flex';
     document.getElementById('TOP').style.width = boardGenerator.WIDTH * 24 + 6 + 'px';
+    document.getElementById('end-menu').style.width = boardGenerator.WIDTH * 24 + 6 + 'px';
+}
+
+
+function again(){ window.location.reload(); }
+
+
+function showScores(){
+
+    document.getElementById('scores').innerHTML = '';
+
+    let sorted = scores.getSortedByTime();
+
+    let tr = document.createElement('tr');
+
+    let td1 = document.createElement('td');
+        td1.innerHTML = 'NICK';
+
+    let td2 = document.createElement('td');
+        td2.innerHTML = 'TIME';
+
+    let td3 = document.createElement('td');
+        td3.innerHTML = 'MODE'
+
+    tr.appendChild(td1);
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+
+    document.getElementById('scores').appendChild(tr);
+    
+
+    let max = sorted.length > 10 ? 10 : sorted.length;
+
+    for(let i=0; i<max; i++){
+
+        let tr = document.createElement('tr');
+
+        let td1 = document.createElement('td');
+            td1.innerHTML = sorted[i].nick;
+            td1.classList.add('scr-tab');
+
+        let m, s, ms;
+
+        ms = sorted[i].time;
+        m = Math.floor(ms / 60000);
+
+        ms -= (m * 60000);
+
+        s = Math.floor(ms / 1000);
+
+        ms -= (s * 1000);
+
+        let timeStr = (m < 10 ? '0' + m : m) + ':' + (s < 10 ? '0' + s : s) + ':' + ms;
+        
+        let td2 = document.createElement('td');
+            td2.innerHTML = timeStr;
+            td2.classList.add('scr-tab');
+        
+        let td3 = document.createElement('td');
+            td3.innerHTML = sorted[i].mode;
+            td3.classList.add('scr-tab');
+
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+
+        document.getElementById('scores').appendChild(tr);
+    }
+
 }
 
 window.addEventListener('contextmenu', (ev) => { ev.preventDefault(); })
